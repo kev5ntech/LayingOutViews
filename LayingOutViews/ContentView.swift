f5879e8a0593922e1cce9d985b758179fd3ae2a1
Kevin Wong <kev5ntech@gmail.com>
2024-10-17 15:00:46 -0700

    Initial Commit

---
+++ LayingOutViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LayingOutViews
+//
+//  Created by Kevin Wong on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LayingOutViews/LayingOutViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayingOutViewsApp.swift
+//  LayingOutViews
+//
+//  Created by Kevin Wong on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LayingOutViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
