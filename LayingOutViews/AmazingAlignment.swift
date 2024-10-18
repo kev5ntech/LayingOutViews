459c33faa907a0256841d01cecd4061cb1629a1d
Kevin Wong <kev5ntech@gmail.com>
2024-10-17 17:04:32 -0700

    Added Amazing alingment View

--- LayingOutViews/AmazingAlignment.swift
+++ LayingOutViews/AmazingAlignment.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct AmazingAlignment: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading) {
+            Image(systemName: "books.vertical.fill")
+                .font(.system(size: 40))
+            Rectangle().frame(maxWidth: .infinity, maxHeight: 10)
+            VStack (alignment: .trailing) {
+                Image(systemName: "books.vertical.fill")
+                    .font(.system(size: 40))
+                Rectangle().frame(maxWidth: .infinity, maxHeight: 10)
+            }
+            Image(systemName: "books.vertical.fill")
+                    .font(.system(size: 40))
+                    .frame(alignment: .leading)
+            Rectangle().frame(maxWidth: .infinity, maxHeight: 10)
+            VStack (alignment: .trailing) {
+                HStack {
+                    Image(systemName: "books.vertical.fill")
+                        .font(.system(size: 40))
+                    Image(systemName: "books.vertical.fill")
+                        .font(.system(size: 40))
+                }
+                Rectangle().frame(maxWidth: .infinity, maxHeight: 10)
+            }
+            HStack (alignment: .top) {
+                    Image(systemName: "books.vertical.fill")
+                        .font(.system(size: 40))
+                }
+            Rectangle().frame(maxWidth: .infinity, maxHeight: 10)
+        }.padding(.top)
+            .frame(width: 250)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 4)
+            
     }
 }
 
