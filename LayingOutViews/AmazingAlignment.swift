80afb39625cc5b89e844faf8f4cf4f656a9ad238
Kevin Wong <kev5ntech@gmail.com>
2024-10-17 17:00:18 -0700

    Added Amazing Alignment view

---
+++ LayingOutViews/AmazingAlignment.swift
@@ -0,0 +1,18 @@
+//
+//  AmazingAlignment.swift
+//  LayingOutViews
+//
+//  Created by Kevin Wong on 10/17/24.
+//
+
+import SwiftUI
+
+struct AmazingAlignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AmazingAlignment()
+}
