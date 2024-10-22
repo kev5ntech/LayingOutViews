9ceaa153d1f7d3006b10e4c983d956ee33bfde78
Kevin Wong <kev5ntech@gmail.com>
2024-10-21 21:54:21 -0700

    Adding Style and look

--- LayingOutViews/RatingView.swift
+++ LayingOutViews/RatingView.swift
@@ -16,6 +16,8 @@ struct RatingView: View {
                 Text("What's your mood?")
                     .foregroundColor(.brown)
                     .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
                 HStack {
                     ForEach(emojis, id: \.self) { emoji in
                         Button {
@@ -23,15 +25,19 @@ struct RatingView: View {
                         } label: {
                             VStack {
                                 Text(emoji)
-                                    .font(.system(size: 35))
+                                    .font(.system(size: 35)).padding(.bottom, 5)
+                                    .frame(maxWidth: .infinity, alignment: .center)
                                 Image(systemName: value == emoji ? "circle.fill" : "circle")
                                     .font(.system(size: 16))
                                     .foregroundColor(.brown)
-                            }
+                            }.padding()
                         }
                     }
-                }
-            }
+                }.frame(maxHeight: .infinity, alignment: .center)
+            }.frame(minHeight: 100, maxHeight: 250)
+                .background(Color.teal)           // Changing the background color
+                .foregroundColor(.brown)           // Setting text color for better contrast
+                .cornerRadius(20)
         }
 }
 
